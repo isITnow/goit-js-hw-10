d93a69c2f33651d8cfb5cf02c00ce7a732a16858
Roman Serediuk <box4rom@gmail.com>
2022-08-15 17:31:41 +0300

    task completed

---
+++ src/fetchCountries.js
@@ -0,0 +1,10 @@
+export const fetchCountries = name => {
+    return fetch(
+        `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`,
+    ).then(resp => {
+        if (!resp.ok) {
+            throw new Error(resp.status);
+        }
+        return resp.json();
+    });
+};
--- src/index.js
+++ src/index.js
@@ -1,5 +1,54 @@
 import './css/styles.css';
 import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
+import { fetchCountries } from './fetchCountries.js';
+import countryList from './handlebars_files/country_list.hbs';
+import countryInfo from './handlebars_files/country_info.hbs';
 
 const DEBOUNCE_DELAY = 300;
+
+const refs = {
+    input: document.querySelector('#search-box'),
+    countryList: document.querySelector('.country-list'),
+    countryInfo: document.querySelector('.country-info'),
+};
+
+refs.input.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
+
+function onInput(evt) {
+    const country = evt.target.value.toLowerCase().trim();
+    if (country.length <= 0) {
+        refs.countryList.innerHTML = '';
+        refs.countryInfo.innerHTML = '';
+        return;
+    }
+    //   console.log(fetchCountries(country));
+    fetchCountries(country)
+        .then(data => {
+            if (data.length > 10) {
+                Notiflix.Notify.info(
+                    'Too many matches found. Please enter a more specific name.',
+                );
+            } else if (data.length >= 2 && data.length <= 10) {
+                createListCard(data);
+            } else {
+                createInfoCard({
+                    ...data[0],
+                    languages: Object.values(data[0].languages).join(', '),
+                });
+            }
+        })
+        .catch(error =>
+            Notiflix.Notify.failure('Oops, there is no country with that name'),
+        );
+}
+
+function createListCard(countries) {
+    refs.countryInfo.innerHTML = '';
+    refs.countryList.insertAdjacentHTML('afterbegin', countryList(countries));
+}
+
+function createInfoCard(country) {
+    refs.countryList.innerHTML = '';
+    refs.countryInfo.innerHTML = countryInfo(country);
+}
